97eea60951b9bc0c6513ae0a0ece88c6eefddedd
Krich2020 <keston@therichfamily.net>
2024-01-05 09:15:01 -0700

    added delete and update requests for user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const Thought = require("../models/Thought");
 
 module.exports = {
   async getUsers(req, res) {
@@ -28,6 +29,48 @@ module.exports = {
     try {
       const dbUserData = await User.create(req.body);
       res.json(dbUserData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async updateUser(req, res) {
+    try {
+      const dbUserData = await User.findOneAndUpdate(
+        req.params.userId,
+        req.body,
+        {
+          new: true,
+        }
+      );
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user with that ID" });
+        return;
+      }
+      res.json(dbUserData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async deleteUser(req, res) {
+    try {
+      const dbUserData = await User.findOne({
+        _id: req.params.userId,
+      });
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user with that ID" });
+        return;
+      }
+
+      const thoughts = await Thought.find({
+        username: dbUserData.username,
+      });
+      for (const thought of thoughts) {
+        await thought.remove();
+      }
+
+      const deleteUser = await User.delete(dbUserData);
+
+      res.json({ message: "User Deleted" });
     } catch (err) {
       res.status(500).json(err);
     }
