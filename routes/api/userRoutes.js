e26f5cd50eb132113ae9951d65d39288af5d5626
Krich2020 <keston@therichfamily.net>
2024-01-05 10:30:13 -0700

    created api routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+} = require("../../controllers/userControllers.js");
+
+router.route("/").get(getUsers).post(createUser);
+
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route("/:userId/friends/:friendId").post(addFriend);
